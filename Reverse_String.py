3f0de18f13feeead907c3e22bb26147832799042
21a91a05h6 <97424402+21a91a05h6@users.noreply.github.com>
2022-07-02 22:26:03 +0530

    s=input() for i in range(len(s)-1,-1,-1):     if(s[i]==' '):         c=0         for j in range(i+1,len(s)):             if(s[j]==' '):                 c+=1             if(c==1):                 break             else:                 print(s[j],end='')         print(' ',end='')     if(i==0):         for j in range(0,len(s)):             if(s[j]==' '):                 break             print(s[j],end='')

--- Reverse_String.py
+++ Reverse_String.py
@@ -1,7 +1,17 @@
-def reverse(s):
-    str=''
-    for i in s:
-        str=i+str
-    return str
 s=input()
-print(reverse(s))
+for i in range(len(s)-1,-1,-1):
+    if(s[i]==' '):
+        c=0
+        for j in range(i+1,len(s)):
+            if(s[j]==' '):
+                c+=1
+            if(c==1):
+                break
+            else:
+                print(s[j],end='')
+        print(' ',end='')
+    if(i==0):
+        for j in range(0,len(s)):
+            if(s[j]==' '):
+                break
+            print(s[j],end='')
